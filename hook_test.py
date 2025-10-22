aa6fdb5fe0e2445485f3bc3b22b67a606f2f1f4f
Salavat Khidiyatullin <salavat220499@gmail.com>
2025-10-22 19:09:45 +0300

    add_hook_test_file

---
+++ hook_test.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+import requests
+
+
+url = "http://127.0.0.1:8000/webhook/ticket"
+body = {
+  "chat_id": "398039301",
+  "username": "sam_tvls",
+  "question": "Цена проживания?",
+  "ai_confident": False,
+  "external_id": "",
+  "date": int(datetime.now().timestamp() * 1000)
+}
+
+
+if __name__ == "__main__":
+    res = requests.post(url=url, json=body)
+    print(res)
