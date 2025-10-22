74d7c7d3590f670b289933b25fe21116eba89655
Salavat Khidiyatullin <salavat220499@gmail.com>
2025-10-22 20:09:36 +0300

    update_server_listening

--- n8n_webhook.py
+++ n8n_webhook.py
@@ -74,7 +74,8 @@ async def health_check():
 
 
 async def run_n8n_webhook():
-    config = uvicorn.Config(app, host="127.0.0.1", port=5090, log_level="info")
+    config = uvicorn.Config(app, host="0.0.0.0", port=5090, log_level="info")
     server = uvicorn.Server(config)
     await server.serve()
+
 
